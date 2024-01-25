4f781731477710e0a4a29f6c1f7364ed57af99d6
ReganWiggins <155993524+ReganWiggins@users.noreply.github.com>
2024-01-25 11:34:47 -0600

    Add files via upload

---
+++ Javascript Opp.js
@@ -0,0 +1,96 @@
+// Declare variables a and b, and assign values
+let a = 20;
+let b = 4;
+
+// Declare a variable add and store the result of adding a and b
+let add = a + b;
+
+// Declare a variable minus and store the result of subtracting b from a
+let minus = a - b;
+
+// Declare a variable multiply and store the result of multiplying a and b
+let multiply = a * b;
+
+// Declare a variable dividing and store the result of dividing a by b
+let dividing = a / b;
+
+// Log the results to the console
+console.log("Addition:", add);
+console.log("Subtraction:", minus);
+console.log("Multiplication:", multiply);
+console.log("Division:", dividing);
+
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Myelle";
+let lastName = "Jacks";
+
+// What is the value of: num + str?
+console.log("num + str:", num + str);
+
+// What is the value of: num + str2?
+console.log("num + str2:", num + str2);
+
+// What is the value of: num + isPresent?
+console.log("num + isPresent:", num + isPresent);
+
+// What is the value of: firstName + num?
+console.log("firstName + num:", firstName + num);
+
+// What is the value of: isPresent + str?
+console.log("isPresent + str:", isPresent + str);
+
+// What is the value of: firstName + lastName?
+console.log("firstName + lastName:", firstName + lastName);
+
+num + str: 1111
+num + str2: 11eleven
+num + isPresent: 12
+firstName + num: Myelle11
+isPresent + str: Jacks11
+firstName + lastName:Myelle Jacks
+
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+
+// What is the value of: val == str3?
+console.log("val == str3:", val == str3);
+
+// What is the value of: val === str3?
+console.log("val === str3:", val === str3);
+
+// What is the value of: !isPresent2?
+console.log("!isPresent2:", !isPresent2);
+
+// What is the value of: (“eleven” == str4 && val >= str3)?
+console.log("(“eleven” == str4 && val >= str3):", ("eleven" == str4 && val >= str3));
+
+// What is the value of: (!isPresent2 || isPresent2)?
+console.log("(!isPresent2 || isPresent2):", (!isPresent2 || isPresent2));
+
+// What is the value of: 0 == false?
+console.log("0 == false:", 0 == false);
+
+// What is the value of: 0 === false?
+console.log("0 === false:", 0 === false);
+
+// What is the value of: 0 != false?
+console.log("0 != false:", 0 != false);
+
+// What is the value of: 0 !== false?
+console.log("0 !== false:", 0 !== false);
+Results:
+
+val == str3: true
+val === str3: false
+!isPresent2: true
+(“eleven” == str4 && val >= str3): false
+(!isPresent2 || isPresent2): true
+0 == false: true
+0 === false: false
+0 != false: false
+0 !== false: true
